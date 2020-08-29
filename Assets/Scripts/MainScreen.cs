99cddb7fa2319caadf957328f93d7b516612884d
xiarang <xiarang94925@163.com>
2020-08-29 20:11:23 +0430

    dynamic tiles initilizing finished

---
+++ Assets/Scripts/MainScreen.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainScreen : MonoBehaviour
+{
+    [SerializeField] SpriteRenderer[] boardTiles;
+    [SerializeField] Sprite[] resources;
+    string[] res = { "field", "forest", "hill", "mountain", "pasture" };
+    // Start is called before the first frame update
+    void Start()
+    {
+        initTiles();
+    }
+
+    private void initTiles() { 
+    
+        List<string> tilesRes = new List<string>() { "desert" };
+        for (int i = 1; i < 19; i++) { 
+            tilesRes.Add(res[Random.Range(0, 5)]);
+            var r = Random.Range(0, 5);
+            boardTiles[i].sprite = resources[r];
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Test : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        Debug.Log(gameObject.ToString() + "ali");
+    }
+}
