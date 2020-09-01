a1f4cc8fc11ae1c6e6ed36022c52cfa84156a183
xiarang <xiarang94925@163.com>
2020-09-01 20:16:32 +0430

    initializing counter compenent

---
+++ Assets/Scripts/Counter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Counter : MonoBehaviour
+{
+    [SerializeField] int currentNumber = 0;
+
+    public void OnPlusPressed()
+    {
+        Debug.Log(currentNumber.ToString());
+        currentNumber++;
+        SetText();
+    }
+
+    public void OnMinusPressed()
+    {
+        Debug.Log(currentNumber.ToString());
+        currentNumber--;
+        SetText();
+    }
+
+    public void SetText()
+    {
+        var textComponent = GetComponent<TextMeshProUGUI>();
+        textComponent.text = (currentNumber).ToString();
+    }
+
+    void Start()
+    {
+    }
+}
